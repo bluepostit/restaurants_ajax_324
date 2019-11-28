eab4d37ab488d44fcfbf964dca009d929433828e
bluepostit <32864499+bluepostit@users.noreply.github.com>
2019-11-28 11:05:23 +0200

    JS views: review create, rest delete

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,9 +4,15 @@ class ReviewsController < ApplicationController
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review.restaurant = @restaurant
     if @review.save
-      redirect_to restaurant_path(@restaurant)
+      respond_to do |format|
+        format.html { redirect_to restaurant_path(@restaurant) }
+        format.js
+      end
     else
-      render 'restaurants/show'
+      respond_to do |format|
+        format.html { render 'restaurants/show' }
+        format.js
+      end
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  resources :restaurants, only: %i[index show] do
+  resources :restaurants, only: %i[index show destroy] do
     resources :reviews, only: %i[create]
   end
 end
